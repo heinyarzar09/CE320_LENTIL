37538abcca5abc422b39e2b2027037d20cd4f28d
Hein Yar Zar <yarzar9241@gmail.com>
2024-07-24 20:19:37 +0800

    Store User Details in Database

---
+++ app/models.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from . import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
